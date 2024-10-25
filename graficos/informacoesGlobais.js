9fbd587d2e980cc0e4fa91d117f305b7c8e2d508
Joao Batista de Souza Pedrosa <114430297+Jobapedrosa@users.noreply.github.com>
2024-10-24 21:14:57 -0300

    tratei

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,12 +4,17 @@ async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     //console.log(dados)
+    
+    const pessoasMundo = (dados.total_pessoas_mundo / 1e9)
+    const pessoasComAcessoAEducacao = (dados.total_pessoas_com_acesso_a_educacao / 1e9)
+    const horas = parseInt(dados.tempo_medio_dia_estudando)
+    const pessoasComEducacoSuperior = (dados.total_pessoas_com_educacao_superior / 1e9)
+    
+
+
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-
-    paragrafo.innerHTML =  `Você sabia que o total total de pessoas no mundo é de <span>${dados.total_pessoas_mundo}</span> de pessoas e que o  total de pessoas com acesso à educação é de <span>${dados.total_pessoas_com_acesso_a_educacao}</span>? O tempo médio gasto por dia estudando é de <span>${dados.tempo_medio_dia_estudando} horas </span> e o total de pessoas com educação superior é de <span>${dados.total_pessoas_com_educacao_superior}</span>.  Será que esses dados apontam para uma realidade de um mundo esclarecido e com facilidade de acesso à Educação?`
-   
-
+    paragrafo.innerHTML =  `Você sabia que o total total de pessoas no mundo é de <span>${pessoasMundo}</span> de pessoas e que o  total de pessoas com acesso à educação é de <span>${pessoasComAcessoAEducacao}</span>? O tempo médio gasto por dia estudando é de <span>${horas} horas </span> e o total de pessoas com educação superior é de <span>${pessoasComEducacoSuperior}</span>.  Será que esses dados apontam para uma realidade de um mundo esclarecido e com facilidade de acesso à Educação?` 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 }
